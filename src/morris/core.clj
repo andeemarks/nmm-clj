00c9db01ca913c3767fde3781ed4d01a8076f50a
Andy Marks <amarks@thoughtworks.com>
2017-06-23 11:09:44 +1000

    Game state can now be updated and moves checked

--- src/morris/core.clj
+++ src/morris/core.clj
@@ -4,60 +4,69 @@
 		[loom.io :refer :all]
 		))
 
-(def board (graph [:a1 :d1]
-			[:d1 :g1]
-			[:g1 :g4]
-			[:g4 :g7]
-			[:g7 :d7]
-			[:d7 :a7]
-			[:a7 :a4]
-			[:a4 :a1]
-			[:c3 :d3]
-			[:c3 :c4]
-			[:d3 :e3]
-			[:e3 :e4]
-			[:c4 :c5]
-			[:e4 :e5]
-			[:c5 :d5]
-			[:d5 :e5]
-			[:b2 :d2]
-			[:d2 :f2]
-			[:b2 :b4]
-			[:b4 :b6]
-			[:b6 :d6]
-			[:d6 :f6]
-			[:f6 :f4]
-			[:f4 :f2]
-			[:b4 :c4]
-			[:f4 :e4]
-			[:d2 :d3]
-			[:d6 :d5]
-			[:d1 :d2]
-			[:a4 :b4]
-			[:d7 :d6]
-			[:g4 :f4]))
+(defn make-white-piece [id] (str :white "-" id))
+(defn make-black-piece [id] (str :black "-" id))
+
+(defn make-location [id] id)
+
+(defn connect-from-to [from to]
+	[(make-location from) (make-location to)])
+
+(defn- board [] 
+	(graph 
+		; outer square
+		(connect-from-to :a1 :d1) (connect-from-to :d1 :g1) (connect-from-to :g1 :g4) (connect-from-to :g4 :g7) (connect-from-to :g7 :d7) (connect-from-to :d7 :a7) (connect-from-to :a7 :a4) (connect-from-to :a4 :a1)
+		; inner square
+		(connect-from-to :c3 :d3) (connect-from-to :c3 :c4) (connect-from-to :d3 :e3) (connect-from-to :e3 :e4) (connect-from-to :c4 :c5) (connect-from-to :e4 :e5) (connect-from-to :c5 :d5) (connect-from-to :d5 :e5)
+		; middle square
+		(connect-from-to :b2 :d2) (connect-from-to :d2 :f2) (connect-from-to :b2 :b4) (connect-from-to :b4 :b6) (connect-from-to :b6 :d6) (connect-from-to :d6 :f6) (connect-from-to :f6 :f4) (connect-from-to :f4 :f2)
+		; connectors
+		(connect-from-to :b4 :c4) (connect-from-to :f4 :e4) (connect-from-to :d2 :d3) (connect-from-to :d6 :d5) (connect-from-to :d1 :d2) (connect-from-to :a4 :b4) (connect-from-to :d7 :d6) (connect-from-to :g4 :f4)))
+
+(defn init-game []
+	(let [board (board)
+				white-pieces (map #(make-white-piece %) (range 1 9))
+				black-pieces (map #(make-black-piece %) (range 1 9))
+				game-state nil]
+				{:board board 
+					:white-pieces white-pieces 
+					:black-pieces black-pieces 
+					:game-state game-state}))
+
+(defn make-mill [loc1 loc2 loc3]
+	(subgraph (board) [(make-location loc1) (make-location loc2) (make-location loc3)]))
 
 (def mills [
-		(subgraph board [:a1 :d1 :g1])
-		(subgraph board [:b2 :d2 :f1])
-		(subgraph board [:c3 :d3 :e3])
-		(subgraph board [:a4 :b4 :c4])
-		(subgraph board [:e4 :f4 :g4])
-		(subgraph board [:c5 :d5 :e5])
-		(subgraph board [:b6 :d6 :f6])
-		(subgraph board [:a7 :d7 :g7])
-		(subgraph board [:a1 :a4 :a7])
-		(subgraph board [:b2 :b4 :b6])
-		(subgraph board [:c3 :c4 :c5])
-		(subgraph board [:d1 :d2 :d3])
-		(subgraph board [:d5 :d6 :d7])
-		(subgraph board [:e3 :e4 :e5])
-		(subgraph board [:f2 :f4 :f6])
-		(subgraph board [:g1 :g4 :g7])
-		])
+	; horizontal mills
+	(make-mill :a1 :d1 :g1)
+	(make-mill :b2 :d2 :f1)
+	(make-mill :c3 :d3 :e3)
+	(make-mill :a4 :b4 :c4)
+	(make-mill :e4 :f4 :g4)
+	(make-mill :c5 :d5 :e5)
+	(make-mill :b6 :d6 :f6)
+	(make-mill :a7 :d7 :g7)
+	; vertical mills
+	(make-mill :a1 :a4 :a7)
+	(make-mill :b2 :b4 :b6)
+	(make-mill :c3 :c4 :c5)
+	(make-mill :d1 :d2 :d3)
+	(make-mill :d5 :d6 :d7)
+	(make-mill :e3 :e4 :e5)
+	(make-mill :f2 :f4 :f6)
+	(make-mill :g1 :g4 :g7)
+	])
+
 
 (defn completed-mill? [mills]
 	(= 3 ((frequencies mills) true)))
 
 (defn potential-mill? [mills]
 	(= 1 ((frequencies mills) false)))
+
+(defn update-game [game piece destination]
+	(let [current-game-state (:game-state game)
+				destination-available? (nil? (destination current-game-state))]
+		(if destination-available?
+			(assoc game :game-state (merge current-game-state {destination piece}))
+			(throw (IllegalStateException. (str "Location " destination " is already occupied"))))))
--- test/morris/core_test.clj
+++ test/morris/core_test.clj
@@ -2,7 +2,18 @@
   (:require [midje.sweet :refer :all]
             [morris.core :refer :all]))
 
-(facts "completed-mill?-mills"
+(facts "moving pieces"
+  (fact "is legal if the location is unoccupied"
+    (let [game (init-game)]
+      (update-game game (first (:white-pieces game)) :a1) =not=> nil))
+  (fact "is illegal if the location is occupied"
+    (let [game (init-game)
+          updated-game (update-game game (first (:white-pieces game)) :a1)
+          piece (first (:black-pieces updated-game))]      
+      (update-game updated-game piece :a1) => (throws IllegalStateException)))
+  )
+
+(facts "completed-mills"
   (fact "have all three positions occupied"
     (completed-mill? [true, true, true]) => true
     (completed-mill? [false, true, true]) => false
@@ -12,7 +23,7 @@
     (completed-mill? [false, false, false]) => false
 ))
 
-(facts "potential-mill?-mills"
+(facts "potential-mills"
   (fact "have one position unoccupied"
     (potential-mill? [true, true, true]) => false
     (potential-mill? [false, true, true]) => true
