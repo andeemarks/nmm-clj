3d70c65b75a9f7b67c6d0eba2521415bf03c6364
Andy Marks <amarks@thoughtworks.com>
2017-07-21 13:01:40 +1000

    DRYing end game and mill completion event handling

--- src/morris/core.clj
+++ src/morris/core.clj
@@ -21,15 +21,24 @@
 		(assoc-in [:game-state destination] piece-to-move)
 		(update-in [:game-state] dissoc origin)))
 
+(defn- handle-mill-completion-event [game destination]
+	(let [completed-mills (mill/find-completed-mills (:game-state game) destination)]
+		(if completed-mills
+			(assoc game :completed-mill-event (first completed-mills))
+			(assoc game :completed-mill-event nil))))
+
+(defn- handle-end-game-event [game]
+	(let [game-finished? (board/end-game? (:white-pieces game) (:black-pieces game) (:game-state game))]
+		(if game-finished?
+			(assoc game :game-over-event true)
+			(assoc game :game-over-event nil))))
+
 (defn move-piece [game origin destination]
 	(let [piece-to-move (origin (:game-state game))]
 		(if (board/valid-move? (:current-player game) (:game-state game) origin destination)
-			(let [new-game (update-game-for-move game piece-to-move origin destination)
-						mill-completed? (mill/find-completed-mills (:game-state new-game) destination)]
+			(let [new-game (update-game-for-move game piece-to-move origin destination)]
 				(log/info "Moving " piece-to-move " from " origin " to " destination)
-				(if mill-completed?
-					(assoc new-game :completed-mill-event (first mill-completed?))
-					(assoc new-game :completed-mill-event nil)))
+				(handle-mill-completion-event new-game destination))
 			(throw (IllegalStateException. (str "Cannot move " piece-to-move " from " origin " to " destination))))))
 
 (defn place-piece [game piece destination]
@@ -39,12 +48,9 @@
 			(if (board/location-available? destination current-game-state)
 				(let [new-game-state (merge current-game-state {destination piece})
 							game-with-updated-player-pools (remove-piece-from-pool game piece)
-							new-game (assoc game-with-updated-player-pools :game-state new-game-state)
-							mill-completed? (mill/find-completed-mills new-game-state destination)]
+							new-game (assoc game-with-updated-player-pools :game-state new-game-state)]
 					(log/info "Placing " piece " on " destination)
-					(if mill-completed?
-						(assoc new-game :completed-mill-event (first mill-completed?))
-						(assoc new-game :completed-mill-event nil)))
+					(handle-mill-completion-event new-game destination))
 				(throw (IllegalStateException. (str "Location " destination " is already occupied"))))
 			(throw (IllegalArgumentException. (str "Location " destination " does not exist on board"))))))
 
@@ -52,10 +58,7 @@
 	(if (board/valid-removal? (:current-player game) location-containing-piece (:game-state game))
 		(let [updated-game (-> game
 												(update-in [:game-state] dissoc location-containing-piece)
-												(dissoc :completed-mill-event))
-					game-finished? (board/end-game? (:white-pieces updated-game) (:black-pieces updated-game) (:game-state updated-game))]
+												(dissoc :completed-mill-event))]
 			(log/info "Removing piece from " location-containing-piece)
-			(if game-finished?
-				(assoc updated-game :game-over-event true)
-				(assoc updated-game :game-over-event nil))) 
+			(handle-end-game-event updated-game))
 		(throw (IllegalArgumentException. (str "Location " location-containing-piece " cannot be removed by " (:current-player game))))))
