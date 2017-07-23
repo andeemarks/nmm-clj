eeafe12451d6c7a80a2bc9656aa9ca6c792048ab
Andy Marks <amarks@thoughtworks.com>
2017-07-23 20:48:18 +1000

    Moving mill data structures to mill.clj

--- src/morris/be/mill.clj
+++ src/morris/be/mill.clj
@@ -1,7 +1,32 @@
 (ns morris.be.mill
   (:require [loom.graph :as graph]
+            [loom.graph :refer :all]
             [morris.common.board :as board]
             [morris.be.piece :as piece]))
+
+(defn make-mill [loc1 loc2 loc3]
+	(subgraph (board/board) [loc1 loc2 loc3]))
+
+(def mills [
+	; horizontal mills
+	(make-mill :a1 :d1 :g1)
+	(make-mill :b2 :d2 :f2)
+	(make-mill :c3 :d3 :e3)
+	(make-mill :a4 :b4 :c4)
+	(make-mill :e4 :f4 :g4)
+	(make-mill :c5 :d5 :e5)
+	(make-mill :b6 :d6 :f6)
+	(make-mill :a7 :d7 :g7)
+	; vertical mills
+	(make-mill :a1 :a4 :a7)
+	(make-mill :b2 :b4 :b6)
+	(make-mill :c3 :c4 :c5)
+	(make-mill :d1 :d2 :d3)
+	(make-mill :d5 :d6 :d7)
+	(make-mill :e3 :e4 :e5)
+	(make-mill :f2 :f4 :f6)
+	(make-mill :g1 :g4 :g7)
+	])
 
 (defn occupied-by-same-colour-pieces? [mill game-state]
 	(let [nodes-in-mill (graph/nodes mill)
@@ -32,6 +57,6 @@
 	(graph/has-node? mill recent-move))
 
 (defn find-completed-mills [game-state recent-move]
-	(let [relevant-mills (filter #(mill-contains-recent-move? recent-move %) board/mills)
+	(let [relevant-mills (filter #(mill-contains-recent-move? recent-move %) mills)
 				just-completed-mills (map #(check-for-completed-mill % game-state) relevant-mills) ]
 		(remove nil? just-completed-mills)))
--- src/morris/common/board.clj
+++ src/morris/common/board.clj
@@ -5,46 +5,17 @@
             [morris.be.piece :as piece]
             [taoensso.timbre :as log]))
 
-(defn make-location [id] id)
-
-(defn connect-from-to [from to]
-	[(make-location from) (make-location to)])
-
 (defn board [] 
 	(graph 
 		; outer square
-		(connect-from-to :a1 :d1) (connect-from-to :d1 :g1) (connect-from-to :g1 :g4) (connect-from-to :g4 :g7) (connect-from-to :g7 :d7) (connect-from-to :d7 :a7) (connect-from-to :a7 :a4) (connect-from-to :a4 :a1)
+		[ :a1 :d1] [ :d1 :g1] [ :g1 :g4] [ :g4 :g7] [ :g7 :d7] [ :d7 :a7] [ :a7 :a4] [ :a4 :a1]
 		; inner square
-		(connect-from-to :c3 :d3) (connect-from-to :c3 :c4) (connect-from-to :d3 :e3) (connect-from-to :e3 :e4) (connect-from-to :c4 :c5) (connect-from-to :e4 :e5) (connect-from-to :c5 :d5) (connect-from-to :d5 :e5)
+		[ :c3 :d3] [ :c3 :c4] [ :d3 :e3] [ :e3 :e4] [ :c4 :c5] [ :e4 :e5] [ :c5 :d5] [ :d5 :e5]
 		; middle square
-		(connect-from-to :b2 :d2) (connect-from-to :d2 :f2) (connect-from-to :b2 :b4) (connect-from-to :b4 :b6) (connect-from-to :b6 :d6) (connect-from-to :d6 :f6) (connect-from-to :f6 :f4) (connect-from-to :f4 :f2)
+		[ :b2 :d2] [ :d2 :f2] [ :b2 :b4] [ :b4 :b6] [ :b6 :d6] [ :d6 :f6] [ :f6 :f4] [ :f4 :f2]
 		; connectors
-		(connect-from-to :b4 :c4) (connect-from-to :f4 :e4) (connect-from-to :d2 :d3) (connect-from-to :d6 :d5) (connect-from-to :d1 :d2) (connect-from-to :a4 :b4) (connect-from-to :d7 :d6) (connect-from-to :g4 :f4)
+		[ :b4 :c4] [ :f4 :e4] [ :d2 :d3] [ :d6 :d5] [ :d1 :d2] [ :a4 :b4] [ :d7 :d6] [ :g4 :f4]
 		))
-
-(defn make-mill [loc1 loc2 loc3]
-	(subgraph (board) [(make-location loc1) (make-location loc2) (make-location loc3)]))
-
-(def mills [
-	; horizontal mills
-	(make-mill :a1 :d1 :g1)
-	(make-mill :b2 :d2 :f2)
-	(make-mill :c3 :d3 :e3)
-	(make-mill :a4 :b4 :c4)
-	(make-mill :e4 :f4 :g4)
-	(make-mill :c5 :d5 :e5)
-	(make-mill :b6 :d6 :f6)
-	(make-mill :a7 :d7 :g7)
-	; vertical mills
-	(make-mill :a1 :a4 :a7)
-	(make-mill :b2 :b4 :b6)
-	(make-mill :c3 :c4 :c5)
-	(make-mill :d1 :d2 :d3)
-	(make-mill :d5 :d6 :d7)
-	(make-mill :e3 :e4 :e5)
-	(make-mill :f2 :f4 :f6)
-	(make-mill :g1 :g4 :g7)
-	])
 
 (defn location-exists? [location]
 	(let [result (has-node? (board) location)]
