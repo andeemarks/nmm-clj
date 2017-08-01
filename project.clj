833b59296fb67fa7efb34432337c7b8e84fd1e17
Andy Marks <vampwillow@gmail.com>
2017-08-01 12:40:44 +1000

    Revert version updates due to errors starting ring server

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                   [io.aviso/pretty "0.1.34"]
                   [com.taoensso/timbre "4.10.0"]
                   [ring-logger-timbre "0.7.5"]
-                  [metosin/compojure-api "1.1.11"]
+                  [metosin/compojure-api "1.1.10"]
                   [http-kit "2.2.0"]
    								[aysylu/loom "1.0.0"]]
 	:main morris.fe.game
@@ -17,7 +17,7 @@
   :profiles {:dev 
               {
                 :plugins [[lein-ring "0.10.0"]]
-                :dependencies [[ring/ring-mock "0.3.1"] [midje "1.8.3"] [http-kit.fake "0.2.2"]]}
+                :dependencies [[ring/ring-mock "0.3.1"] [midje "1.8.3"] [http-kit.fake "0.2.1"]]}
                 :midje {}})
 
   
